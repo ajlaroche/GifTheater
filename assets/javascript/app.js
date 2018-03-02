700fe99c4de53caabe0f20e498ce1323ec5db2ea
Antony Laroche <antony@larochefamily.net>
2018-03-01 23:55:17 -0600

    Add url pull based on button click to console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
-var topics=["NFL","Help Desk","NBA","Game of Thrones"];
+$(document).ready(function () {
+    var topics = ["NFL", "HelpDesk", "NBA", "GameOfThrones"];
+    for (i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        $("#themeButtons").append(newButton);
+        newButton.text(topics[i]).addClass("btn btn-primary").attr("value", topics[i]);
+    }
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+    var apiKey = "&limit=10&rating=g&rating=pg&api_key=Zt8nIPW5T7ucvkGQXASMIT5AVTtE5eA5";
+    var chosenTheme;
+
+    $("#themeButtons").on("click", ".btn", function () {
+        $.ajax({
+            url: queryURL + $(this).attr("value") + apiKey,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response);
+
+        });
+    })
+})
